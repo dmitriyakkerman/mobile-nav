1dea5b34c4832fbc1965aeb475153b282f155a2e
dmitriyakkerman <dmitriyakkerman@gmail.com>
2021-10-28 15:49:06 +0300

    closest polyfill

--- src/js/mobile-nav.js
+++ src/js/mobile-nav.js
@@ -1,3 +1,5 @@
+import './utils/closest';
+
 (function (root, factory) {
   if (typeof define === 'function' && define.amd) {
     define([], factory);
---
+++ src/js/utils/closest.js
@@ -0,0 +1,14 @@
+(function() {
+    if (!Element.prototype.closest) {
+        Element.prototype.closest = function(css) {
+            let node = this;
+
+            while (node) {
+                if (node.matches(css)) return node;
+                else node = node.parentElement;
+            }
+
+            return null;
+        };
+    }
+})();
